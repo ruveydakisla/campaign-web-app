97430e1d4a171324bff2e2be009eeb5ab6cef49b
ruveyda <ruveydakisla34@gmail.com>
2025-05-02 23:39:10 +0300

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Toaster } from "@/components/ui/toaster";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Geist } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
-
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
@@ -37,41 +35,31 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+          <main className="min-h-screen flex flex-col">
+            <div className="flex-1 w-full flex flex-col gap-8">
+              <nav className="w-full border-b border-b-foreground/10">
+                <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center h-auto md:h-16 px-4 md:px-8 py-3 gap-4">
+                  <div className="flex gap-4 items-center font-semibold text-base md:text-sm">
+                    <Link href={"/"}>Campaigns</Link>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <div className="flex-shrink-0">
+                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  </div>
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
+
+              <div className="flex flex-col px-4 sm:px-6 md:px-10 lg:px-16 w-full">
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
+              {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-2 py-4">
+               Footer içerikleri
+            </footer> */}
             </div>
           </main>
         </ThemeProvider>
       </body>
+      <Toaster />
     </html>
   );
 }
